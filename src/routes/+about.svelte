27af5c974e48a82f48a8de3b2b7499cea145f0df
Thomas <thomasmbryson@gmail.com>
2022-12-30 05:26:56 -0800

    small updates

---
+++ src/routes/+about.svelte
@@ -0,0 +1,12 @@
+<div class="about">
+	<h2>About Based Bloat</h2>
+	<p>This website runs faster than most bloatsites but there is room to improve.</p>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+let title = 'Based Bloat Site'
+
+const updateTitle = () => {
+	title = "Based Bloatless Site"
+}
+</script>
+
+<div class="index">
+	<h2>{title}</h2>
+	<p>Will the sveltekit be the kit to end all kits?</p>
+	<button on:click={updateTitle}>make website fast</button>
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
