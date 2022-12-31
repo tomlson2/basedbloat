6ebf41b212024ee68631a56b2ac42721233ab6d9
Thomas <thomasmbryson@gmail.com>
2022-12-31 13:23:48 -0800

    keypress events captured

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,46 @@
 <script>
-	import '../app.css'
-	import { enter } from '../lib/actions' 
-	import { onMount } from 'svelte'
+	import '../app.css';
+	import { enter } from '../lib/actions';
+	import { onMount } from 'svelte';
+        import { onDestroy } from 'svelte';
 	
 	const months = ["january","february","march","april","may","june","july","august","september","october","november","december"];
 	const weekdays = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
 	let day = new Date();
 	
-	let text
-	let input = '';
-	function submit() {
-		input = ''
+	let text = ''
+	let output 
+	function handleKeydown(e) {
+		let charCode =	e.keyCode;
+		if (!((charCode < 48 && charCode != 32) || (charCode > 57 && charCode < 65) || (charCode > 90 && charCode < 97) || charCode > 122)) {
+			text += e.key;
+		}
+		else if (charCode === 13) {
+			handleCommand(text);
+			text = '';
+		}
+		else if (charCode === 8) {
+			text = text.slice(0, -1);
+		}
 	}
-	function init(el){
-		el.focus()
-	}	
+
+	async function handleCommand(cmd) {
+		if (cmd === 'weather') {
+			output = await loadWeather();
+		}
+	}
+
+	async function loadWeather() {
+		const response = await fetch('https://wttr.in/');
+		return { response };
+	}
+
 </script>
 
-<pre class="terminal-block">{input}<span>&nbsp;
+<pre class="terminal-block">{text}<span>&nbsp;
 </span>
 </pre>
-<div>
-<input
-		display: none
-		type="text"
-		spellcheck="false"
-		bind:value={input}
-		use:init
-		use:enter
-		on:enterkey={submit}
-		on:resize={update}
-	/>
-</div>
-
 <style>
-	input {
-		display: none;
-	}
-	body {
-		background-color: #1f1f28
-	}
 	pre {
 		text-align: center;
 		color: #dcd7ba;
@@ -48,5 +49,7 @@
 	pre.terminal-block> span {
 		background-color: hsla(1, 1%, 100%, 1);
 		color: #aaaaa;
-}
+	}
 </style>
+
+<svelte:window on:keydown|preventDefault={handleKeydown} />
