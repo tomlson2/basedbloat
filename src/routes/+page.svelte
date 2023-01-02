7808e9c41118dacefa1e46e2d2540d0853b5fd41
Thomas <thomasmbryson@gmail.com>
2023-01-01 23:59:34 -0800

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,29 @@
 	import Fuse from 'fuse.js';
 	
 	const bookmarks = [
-		{
-			"title": "youtube",
-			"link": "https://youtube.com"
-		},
-		{
-			"title": "reddit",
-			"link": "https://reddit.com"
-		}
+		"https://www.youtube.com",
+		"https://wttr.in",
+		"reddit.com",
+		"github.com",
+		"twitch.tv",
+		"twitter.com",
+		"google.com",
+		"monkeytype.com",
+		"canvas.org",
+		"stackoverflow.com",
+		"op.gg",
+		"libgen.rs",
+		"neetcode.io",
+		"leetcode.com",
+		"levels.fyi"
 	]
 
-	const fuse = new Fuse(bookmarks, {
-		keys: ['title']
-	})
+	const fuse = new Fuse(bookmarks)
 
 	let text = ''
-	let matches = ''
-	let output
+	let matches = fuse.search(".");
+	const urlRegex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)(?:\.[^:\/\n]+)$/; 
+
 
 	function handleKeydown(e) {
 		let charCode =	e.keyCode;
@@ -27,51 +33,76 @@
 			text += e.key;
 		}
 		else if (charCode === 13) {
-			handleCommand(text);
-			matches = '';
-			text = '';
+			try {
+				handleCommand(matches[0].item);
+			} catch (error) {
+				text = '';
+			}
 		}
 		else if (charCode === 8) {
 			text = text.slice(0, -1);
 		}
-		matches = fuse.search(text)[0].item.title
-	}
-
-	async function handleCommand(cmd) {
-		if (cmd === 'weather') {
-			output = await loadWeather();
+		if (text !== '') {
+			matches = fuse.search(text);
+		}
+		else {
+			matches = fuse.search(".");
 		}
 	}
 
-	async function loadWeather() {
-		const response = await fetch('https://wttr.in/');
-		return { response };
+	async function handleCommand(cmd) {
+		if (!/^https?:\/\//.test(cmd)) {
+			cmd = 'http://' + cmd;
+		}
+		document.location.href = cmd;	
+	}
+
+	async function getHtml(url) {
+		try {
+			const response = await fetch(url);
+			const html = await response.text();
+			return html;
+		} catch (error) {
+			console.error(error);
+		}
 	}
 
 </script>
-<h2>tmenu</h2>
-<pre class="terminal-block">{text}<span>&nbsp;
-</span>
-{matches}
-</pre>
+
+<div class="page">
+	<h2>tmenu</h2>
+	<pre class="terminal-block">{text}<span>&nbsp;</span></pre>
+	<span> {#each matches as m} <li class="match">{(m.item).match(urlRegex)[1]}</li> {/each} </span>
+</div>
+
 <style>
-	h2 {
-		text-align: center;
-		position: absolute;
-		bottom: 0;
-		margin-left: 50%;
-		padding: 15px 0;
-		color: #dcd7ba;;
-		font-size: 20px;
-	}
-	pre {
+	.page {
 		text-align: center;
 		color: #dcd7ba;
 		font-size: 40px;
 	}
+	.match:first-child {
+		color: #c34043;
+	}
+	li {
+		list-style: none;
+		font-family: monospace;
+	}
+	h2 {
+		text-align: center;
+		position: absolute;
+		bottom: 0;
+		color: #dcd7ba;;
+		font-size: 20px;
+	}
+	pre {
+		font-size: 40px;
+		display: inline;	
+		margin: 0;
+	}
 	pre.terminal-block> span {
 		background-color: hsla(1, 1%, 60%, 1);
-		color: #aaaaa;
+		color: #aaa;
 	}
 </style>
 
